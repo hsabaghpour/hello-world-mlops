895795fea39ad6d46f3a4eea9b3932cd804d7bab
Alex Sabba <hossein.sabbaghpour@gmail.com>
2026-04-18 14:47:39 -0700

    train.py update

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ import json
 def main():
     iris = load_iris()
     X, y = iris.data, iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     model = LogisticRegression(max_iter=200)
     model.fit(X_train, y_train)
